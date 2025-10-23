977431ce2356a0d82ba7a8876359f79a3ee0132d
Hadyan Putra Yasrizal <ladoijo@gmail.com>
2025-10-23 11:49:42 +0700

    remove console.log

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,4 @@ import serverless from 'serverless-http';
 import { createApp } from './app.js';
 
 const app = createApp();
-
-// app.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`[server] listening on http://localhost:${PORT} (env=${ENV})`);
-// });
-
 export const handler = serverless(app);
